d5a29a948651c8c1739a6d6fa4911cd5e291447a
JQWJ <jangjeong214@naver.com>
2020-11-02 10:58:26 +0900

    'Commit before yarn eject

---
+++ SassComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SassComponent.scss'
+
+const SassComponent = () => {
+    return (
+        <div className="SassComponent">
+            <div classname="box red" />
+            <div classname="box orange" />
+            <div classname="box yellow" />
+            <div classname="box green" />
+            <div classname="box blue" />
+            <div classname="box indigo" />
+            <div classname="box violet" />
+        </div>
+    );
+};
+
+export default SassComponent;
