3ee49ceb1324b25f3cf5d4cfba50169fedcf1625
JQWJ <jangjeong214@naver.com>
2020-11-02 12:23:55 +0900

    second commit

--- App.js
+++ App.js
@@ -1,28 +1,43 @@
+// import React, {Component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header>
+//           <img src={logo} className="logo" alt="logo" />
+//               <p>
+//                 Edit <code>src/App.js</code> and save to reload.
+//               </p>
+//               <a
+//                 className = "App-link"
+//                 href="https://reactjs.org"
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//               >
+//                 Learn React
+//               </a>
+//         </header>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <img src={logo} className="logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className = "App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-        </header>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <StyledComponent />
+            </div>
+        );
+    }
 }
 
 export default App;
